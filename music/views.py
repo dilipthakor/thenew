4b623b8bee30d77ab8f596ce4ce76dbf0adf2351
dilipthakor <thakordili8@gmail.com>
2017-08-31 00:15:38 +0530

    i create templets for views

--- music/views.py
+++ music/views.py
@@ -8,12 +8,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    all_album = Album.objects.all()
-    html = ''
-    for album in all_album:
-        url = '/music/'+str(album.id)+'/'
-        html+= '<a href="' + url+'">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    all_albums = Album.objects.all()
+    context ={'all_albums': all_albums}
+    return render(request,'music/index.html',context)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Detail for album id "+str(album_id)+"</h2>")
