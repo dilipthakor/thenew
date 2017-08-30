0cdc2b004db71cba8ec8d076559b7201140b894f
dilipthakor <thakordili8@gmail.com>
2017-08-30 23:54:34 +0530

    view change as link to id

--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from .models import Album
 #from __future__ import unicode_literals
 
 from django.shortcuts import render
@@ -7,7 +8,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Dilip Thakor </h1>")
+    all_album = Album.objects.all()
+    html = ''
+    for album in all_album:
+        url = '/music/'+str(album.id)+'/'
+        html+= '<a href="' + url+'">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Detail for album id "+str(album_id)+"</h2>")
