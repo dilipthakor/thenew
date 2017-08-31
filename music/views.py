b9f517c28cc704337a543b3f64360c94a6b9e7d8
dilipthakor <thakordili8@gmail.com>
2017-08-31 11:11:47 +0530

    made http 404 response

--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.http import Http404
 from .models import Album
 #from __future__ import unicode_literals
 
@@ -9,10 +10,14 @@ from django.shortcuts import render
 
 def index(request):
     all_albums = Album.objects.all()
-    context ={'all_albums': all_albums}
-    return render(request,'music/index.html',context)
+    return render(request,'music/index.html',{'all_albums': all_albums})
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Detail for album id "+str(album_id)+"</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request,'music/detail.html',{'album': album})
+    
 
 
